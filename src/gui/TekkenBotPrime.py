ac4b5b32a7c4baa2fd1801b541c9f82f0fab7629
dcep93 <dan@quizlet.com>
2019-12-01 13:05:41 -0800

    TekkenBotPrime.py

--- src/gui/TekkenBotPrime.py
+++ src/gui/TekkenBotPrime.py
@@ -14,18 +14,20 @@ from misc.Path import path
 import misc.VersionChecker
 import webbrowser
 
+# todo
+# Frame specific stuff
+
 class TekkenBotPrime(Tk):
     def __init__(self):
         self.overlay = None
 
         self.init_tk()
         self.print_readme()
+        print("Tekken Bot Starting...")
         self.add_menu_cascade()
         self.add_columns_cascade()
         self.add_display_cascade()
-        self.add_color_schema_cascade()
         self.add_mode_cascade()
-        self.add_version_cascade()
         self.configure_grid()
         self.update_launcher()
         self.overlay.hide()
@@ -33,18 +35,16 @@ class TekkenBotPrime(Tk):
     def init_tk(self):
         Tk.__init__(self)
         self.wm_title("dcep93/TekkenBot")
-        self.iconbitmap('./TekkenData/tekken_bot_close.ico')
-
-        self.color_scheme_config = ConfigReader.ConfigReader("color_scheme")
+        self.iconbitmap('./src/TekkenData/tekken_bot_close.ico')
 
         self.menu = Menu(self)
         self.configure(menu=self.menu)
 
         self.text = Text(self, wrap="word")
-        self.stdout = sys.stdout
-        sys.stdout = TextRedirector(self.text, self.stdout, "stdout")
-        self.stderr = sys.stderr
-        sys.stderr = TextRedirector(self.text, self.stderr, "stderr")
+        stdout = sys.stdout
+        sys.stdout = TextRedirector(self.text, stdout, "stdout")
+        stderr = sys.stderr
+        sys.stderr = TextRedirector(self.text, stderr, "stderr")
         self.text.tag_configure("stderr", foreground="#b22222")
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
@@ -58,56 +58,44 @@ class TekkenBotPrime(Tk):
             print("Error reading readme file.")
 
     def add_menu_cascade(self):
-        print("Tekken Bot Starting...")
         self.launcher = FrameDataLauncher(False)
 
         self.overlay = fdo.FrameDataOverlay(self, self.launcher)
 
-        self.tekken_bot_menu = Menu(self.menu)
+        tekken_bot_menu = Menu(self.menu)
 
-        self.menu.add_cascade(label="Tekken Bot", menu=self.tekken_bot_menu)
+        self.menu.add_cascade(label="Tekken Bot", menu=tekken_bot_menu)
 
     def add_columns_cascade(self):
         self.checkbox_dict = {}
-        self.column_menu = Menu(self.menu)
+        column_menu = Menu(self.menu)
         for i, enum in enumerate(fdo.DataColumns):
-            bool = self.overlay.redirector.columns_to_print[i]
-            self.add_checkbox(self.column_menu, enum, "{} ({})".format(enum.name.replace('X', ' ').strip(), fdo.DataColumnsToMenuNames[enum]), bool, self.changed_columns)
-        self.menu.add_cascade(label='Columns', menu=self.column_menu)
+            checked = self.overlay.redirector.columns_to_print[i]
+            name = "{} ({})".format(enum.name.replace('X', ' ').strip(), fdo.DataColumnsToMenuNames[enum])
+            self.add_checkbox(column_menu, enum, name, checked, self.changed_columns)
+        self.menu.add_cascade(label='Columns', menu=column_menu)
 
     def add_display_cascade(self):
-        self.display_menu = Menu(self.menu)
+        display_menu = Menu(self.menu)
         for enum in ovr.DisplaySettings:
             default = self.overlay.tekken_config.get_property(ovr.DisplaySettings.config_name(), enum.name, False)
-            self.add_checkbox(self.display_menu, enum, enum.name, default, self.changed_display)
-        self.menu.add_cascade(label="Display", menu=self.display_menu)
-
-    def add_color_schema_cascade(self):
-        self.color_scheme_menu = Menu(self.menu)
-        self.scheme_var = StringVar()
-        for section in self.color_scheme_config.parser.sections():
-            if section not in ("Comments", "Current"):
-                self.color_scheme_menu.add_radiobutton(label=section, variable=self.scheme_var, value=section, command=lambda : self.changed_color_scheme(self.scheme_var.get()))
-        self.menu.add_cascade(label="Color Scheme", menu=self.color_scheme_menu)
+            self.add_checkbox(display_menu, enum, enum.name, default, self.changed_display)
+        self.menu.add_cascade(label="Display", menu=display_menu)
 
     def add_mode_cascade(self):
-        self.overlay_mode_menu = Menu(self.menu)
+        overlay_mode_menu = Menu(self.menu)
         self.overlay_var = StringVar()
         for mode in OverlayMode:
-            self.overlay_mode_menu.add_radiobutton(label=OverlayModeToDisplayName[mode], variable=self.overlay_var, value=mode.name, command=lambda : self.changed_mode(self.overlay_var.get()))
-        self.menu.add_cascade(label="Mode", menu=self.overlay_mode_menu)
+            label = OverlayModeToDisplayName[mode]
+            command = lambda: self.changed_mode(self.overlay_var.get())
+            overlay_mode_menu.add_radiobutton(label=label,variable=self.overlay_var,value=mode.name,command=command)
+        self.menu.add_cascade(label="Mode", menu=overlay_mode_menu)
         self.mode = OverlayMode.FrameData
-
-    def add_version_cascade(self):
-        self.tekken_bot_menu = Menu(self.menu)
-        self.menu.add_cascade(label="Version", menu=self.tekken_bot_menu)
 
     def configure_grid(self):
         self.text.grid(row = 2, column = 0, columnspan=2, sticky=N+S+E+W)
-        #self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(2, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        #self.grid_columnconfigure(1, weight=1)
 
         self.geometry(str(920) + 'x' + str(720))
 
@@ -117,14 +105,6 @@ class TekkenBotPrime(Tk):
         self.checkbox_dict[lookup_key] = var
         menu.add_checkbutton(label=display_string, onvalue=True, offvalue=False, variable=var, command = button_command)
 
-    def changed_color_scheme(self, section, do_reboot=True):
-        for enum in fdo.ColorSchemeEnum:
-            fdo.CurrentColorScheme.dict[enum] = self.color_scheme_config.get_property(section, enum.name, fdo.CurrentColorScheme.dict[enum])
-            self.color_scheme_config.set_property("Current", enum.name, fdo.CurrentColorScheme.dict[enum])
-        self.color_scheme_config.write()
-        if do_reboot:
-            self.reboot_overlay()
-
     def changed_mode(self, mode):
         self.stop_overlay()
         self.mode = OverlayMode[mode]
@@ -132,13 +112,8 @@ class TekkenBotPrime(Tk):
             self.start_overlay()
 
     def changed_columns(self):
-        generated_columns = []
-        for enum in fdo.DataColumns:
-            var = self.checkbox_dict[enum]
-            generated_columns.append(var.get())
-            if self.mode == OverlayMode.FrameData:
-                self.overlay.update_column_to_print(enum, var.get())
         if self.mode == OverlayMode.FrameData:
+            generated_columns = [self.checkbox_dict[enum].get() for enum in fdo.DataColumns]
             self.overlay.set_columns_to_print(generated_columns)
 
     def changed_display(self):
@@ -153,7 +128,7 @@ class TekkenBotPrime(Tk):
     def stop_overlay(self):
         if self.overlay != None:
             self.overlay.toplevel.destroy()
-        self.overlay = None
+            self.overlay = None
 
     def start_overlay(self):
         if self.mode == OverlayMode.FrameData:
@@ -185,15 +160,16 @@ class TekkenBotPrime(Tk):
             if successful_update:
                 self.overlay.update_state()
         time2 = time.time()
-        elapsed_time = 1000 * (time2 - time1)
         if self.launcher.gameState.gameReader.HasWorkingPID():
-            self.after(max(2, 8 - int(round(elapsed_time))), self.update_launcher)
+            elapsed_time = 1000 * (time2 - time1)
+            wait = max(2, 8 - int(round(elapsed_time)))
         else:
-            self.after(1000, self.update_launcher)
+            wait = 1000
+        self.after(wait, self.update_launcher)
 
     def on_closing(self):
-        sys.stdout = self.stdout
-        sys.stderr = self.stderr
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
         self.destroy()
 
 class TextRedirector(object):
