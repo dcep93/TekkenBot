66fb4d9c5ca5e152ad17d31ea09ee7456430d01a
dcep93 <dan@quizlet.com>
2019-12-02 16:48:20 -0800

    FrameDataOverlay

--- src/gui/FrameDataOverlay.py
+++ src/gui/FrameDataOverlay.py
@@ -1,18 +1,17 @@
+# bad
+
 """
 A transparent frame data display that sits on top of Tekken.exe in windowed or borderless mode.
 """
 
-
-from tkinter import *
-from tkinter.ttk import *
+import enum
+import tkinter
+import tkinter.ttk
 import sys
-from enum import Enum
+
 from . import Overlay
-from .Overlay import CurrentColorScheme, ColorSchemeEnum
 
-from collections import Counter
-
-class DataColumns(Enum):
+class DataColumns(enum.Enum):
     XcommX = 0
     XidX = 1
     name = 3
@@ -46,46 +45,34 @@ DataColumnsToMenuNames = {
 }
 
 class TextRedirector(object):
+    col_max_length = 8
     def __init__(self, stdout, widget, style, fa_p1_var, fa_p2_var):
-        self.stdout = stdout
         self.widget = widget
         self.fa_p1_var = fa_p1_var
         self.fa_p2_var = fa_p2_var
         self.style = style
-        self.widget.tag_config("p1", foreground=CurrentColorScheme.scheme[ColorSchemeEnum.p1_text])
-        self.widget.tag_config("p2", foreground=CurrentColorScheme.scheme[ColorSchemeEnum.p2_text])
-        self.columns_to_print = [True] * len(DataColumns)
 
-        self.style.configure('.', background=CurrentColorScheme.scheme[ColorSchemeEnum.advantage_slight_minus])
+        self.columns_to_print = {i: True for i in DataColumns}
+
+        self.widget.tag_config("p1", foreground=Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.p1_text])
+        self.widget.tag_config("p2", foreground=Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.p2_text])
+
+        self.style.configure('.', background=Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.advantage_slight_minus])
 
     def set_columns_to_print(self, booleans_for_columns):
         self.columns_to_print = booleans_for_columns
-        self.populate_column_names(booleans_for_columns)
+        self.populate_column_names()
 
-    def populate_column_names(self, booleans_for_columns):
-        column_names = ""
+    def populate_column_names(self):
+        column_names = ''
         for i, enum in enumerate(DataColumns):
             col_name = enum.name.replace('X', '')
             col_len = len(col_name)
-            global col_max_length
-            col_max_length = 8
-            if booleans_for_columns[i]:
-
-                if col_len < col_max_length:
-                    if col_len % 2 == 0:
-                        needed_spaces = col_max_length - col_len
-                        col_name = (" " * int(needed_spaces / 2)) + col_name + (" " * int(needed_spaces / 2))
-                    else:
-                        needed_spaces = col_max_length - col_len
-                        col_name = (" " * int(needed_spaces / 2)) + col_name + (" " * int(needed_spaces / 2 + 1))
-
-                
-                col_name = '|' + col_name
-
-                        
-                
-                
-                column_names += col_name
+            if self.columns_to_print[i]:
+                needed_spaces = self.col_max_length - col_len
+                if col_len < self.col_max_length:
+                    col_name = '%s%s%s' % (" " * int(needed_spaces / 2), col_name, " " * int((needed_spaces+1) / 2))
+                column_names += '|%s' % col_name
         self.set_first_column(column_names)
 
     def set_first_column(self, first_column_string):
@@ -94,10 +81,19 @@ class TextRedirector(object):
         self.widget.insert("1.0", first_column_string + '\n')
         self.widget.configure(state="disabled")
 
+    def get_background(self, fa):
+        if fa <= -14:
+            return Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.advantage_very_punishible]
+        elif fa <= -10:
+            return Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.advantage_punishible]
+        elif fa <= -5:
+            return Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.advantage_safe_minus]
+        elif fa < 0:
+            return Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.advantage_slight_minus]
+        else:
+            return Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.advantage_plus]
 
     def write(self, output_str):
-        #self.stdout.write(output_str)
-
         lines = int(self.widget.index('end-1c').split('.')[0])
         max_lines = 5
         if lines > max_lines:
@@ -108,23 +104,13 @@ class TextRedirector(object):
                 self.widget.configure(state="disabled")
 
         if 'NOW:' in output_str:
-
             data = output_str.split('NOW:')[0]
             fa = output_str.split('NOW:')[1][:3]
 
             if '?' not in fa:
-                if int(fa) <= -14:
-                    self.style.configure('.', background=CurrentColorScheme.scheme[ColorSchemeEnum.advantage_very_punishible])
-                elif int(fa) <= -10:
-                    self.style.configure('.', background=CurrentColorScheme.scheme[ColorSchemeEnum.advantage_punishible])
-                elif int(fa) <= -5:
-                    self.style.configure('.', background=CurrentColorScheme.scheme[ColorSchemeEnum.advantage_safe_minus])
-                elif int(fa) < 0:
-                    self.style.configure('.', background=CurrentColorScheme.scheme[ColorSchemeEnum.advantage_slight_minus])
-                else:
-                    self.style.configure('.', background=CurrentColorScheme.scheme[ColorSchemeEnum.advantage_plus])
+                background = self.get_background(int(fa))
+                self.style.configure('.', background=background)
 
-            text_tag = None
             if "p1:" in output_str:
                 self.fa_p1_var.set(fa)
                 data = data.replace('p1:', '')
@@ -136,23 +122,18 @@ class TextRedirector(object):
 
             if '|' in output_str:
                 out = ""
-                for i, col in enumerate(data.split('|')):
-                    if self.columns_to_print[i]:
+                for col in data.split('|'):
+                    if self.columns_to_print[col]:
                         col_value = col.replace(' ', '')
                         col_value_len = len(col_value)
 
-                        if col_value_len < col_max_length:
-                            if col_value_len % 2 == 0:
-                                needed_spaces = col_max_length - col_value_len
-                                col_value = (" " * int(needed_spaces / 2)) + col_value + (" " * int(needed_spaces / 2))
-                            else:
-                                needed_spaces = col_max_length - col_value_len
-                                col_value = (" " * int(needed_spaces / 2 + 1)) + col_value + (" " * int(needed_spaces / 2))
+                        if col_value_len < self.col_max_length:
+                            needed_spaces = self.col_max_length - col_value_len
+                            col_value = '%s%s%s' % (' ' * ((needed_spaces+1) / 2), col_value, ' ' * (needed_spaces / 2))
                         
-                        out += '|' + col_value
+                        out += '|%s' % col_value
 
                 print("\n" + data)
-                    
                         
                 out += "\n"
                 self.widget.configure(state="normal")
@@ -162,34 +143,30 @@ class TextRedirector(object):
                 self.widget.yview('moveto', '.02')
 
 
-
 class FrameDataOverlay(Overlay.Overlay):
     def __init__(self, master, launcher):
-
         self.initialize(master, (1021, 86))
+        self.launcher = launcher
 
         self.show_live_framedata = self.tekken_config.get_property(Overlay.DisplaySettings.tiny_live_frame_data_numbers, True)
 
-        #self.launcher = FrameDataLauncher(self.enable_nerd_data)
-        self.launcher = launcher
+        style = tkinter.ttk.Style()
+        style.theme_use('alt')
+        style.configure('.', background=self.background_color)
+        style.configure('.', foreground=Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.advantage_text])
 
+        # ???
+        tkinter.Grid.columnconfigure(self.toplevel, 0, weight=0)
+        tkinter.Grid.columnconfigure(self.toplevel, 1, weight=0)
+        tkinter.Grid.columnconfigure(self.toplevel, 2, weight=0)
+        tkinter.Grid.columnconfigure(self.toplevel, 3, weight=1)
+        tkinter.Grid.columnconfigure(self.toplevel, 4, weight=0)
+        tkinter.Grid.columnconfigure(self.toplevel, 5, weight=0)
+        tkinter.Grid.columnconfigure(self.toplevel, 6, weight=0)
+        tkinter.Grid.rowconfigure(self.toplevel, 0, weight=1)
+        tkinter.Grid.rowconfigure(self.toplevel, 1, weight=0)
 
-        self.s = Style()
-        self.s.theme_use('alt')
-        self.s.configure('.', background=self.background_color)
-        self.s.configure('.', foreground=CurrentColorScheme.scheme[ColorSchemeEnum.advantage_text])
-
-        Grid.columnconfigure(self.toplevel, 0, weight=0)
-        Grid.columnconfigure(self.toplevel, 1, weight=0)
-        Grid.columnconfigure(self.toplevel, 2, weight=0)
-        Grid.columnconfigure(self.toplevel, 3, weight=1)
-        Grid.columnconfigure(self.toplevel, 4, weight=0)
-        Grid.columnconfigure(self.toplevel, 5, weight=0)
-        Grid.columnconfigure(self.toplevel, 6, weight=0)
-        Grid.rowconfigure(self.toplevel, 0, weight=1)
-        Grid.rowconfigure(self.toplevel, 1, weight=0)
-
-        self.s.configure('TFrame', background=self.tranparency_color)
+        style.configure('TFrame', background=self.tranparency_color)
         self.fa_p1_var, fa_p1_label = self.create_frame_advantage_label(1)
         self.fa_p2_var, fa_p2_label = self.create_frame_advantage_label(5)
 
@@ -202,62 +179,53 @@ class FrameDataOverlay(Overlay.Overlay):
             self.l_live_recovery = self.create_live_recovery(fa_p1_label, 0)
             self.r_live_recovery = self.create_live_recovery(fa_p2_label, 0)
 
-
         self.text = self.create_textbox(3)
 
-        self.stdout = sys.stdout
-        self.redirector = TextRedirector(self.stdout, self.text, self.s, self.fa_p1_var, self.fa_p2_var)
+        stdout = sys.stdout
+        self.redirector = TextRedirector(stdout, self.text, style, self.fa_p1_var, self.fa_p2_var)
         self.text.configure(state="normal")
         self.text.delete("1.0", "end")
-        #self.text.insert("1.0", "{:^5}|{:^8}|{:^9}|{:^7}|{:^5}|{:^5}|{:^8}|{:^5}|{:^5}|{:^7}|{:^5}|{}\n".format(" input ", "type", "startup", "block", "hit", "CH", "active", "track", "tot", "rec", "stun", "notes"))
-        #self.redirector.populate_column_names(self.get_data_columns())
         self.redirector.set_columns_to_print(self.get_data_columns())
 
         self.text.configure(state="disabled")
 
     def get_data_columns(self):
-        booleans_for_columns = []
-        for enum in DataColumns:
-            selected = self.tekken_config.get_property(enum, True)
-            booleans_for_columns.append(selected)
-        return booleans_for_columns
+        return [self.tekken_config.get_property(enum, True) for enum in DataColumns]
 
     def create_padding_frame(self, col):
-        padding = Frame(self.toplevel, width=10)
-        padding.grid(row=0, column=col, rowspan=2, sticky=N + S + W + E)
+        padding = tkinter.ttk.Frame(self.toplevel, width=10)
+        padding.grid(row=0, column=col, rowspan=2, sticky=tkinter.N + tkinter.S + tkinter.W + tkinter.E)
         return padding
 
     def create_live_recovery(self, parent, col):
-        live_recovery_var = StringVar()
+        live_recovery_var = tkinter.StringVar()
         live_recovery_var.set('??')
-        live_recovery_label = Label(parent, textvariable=live_recovery_var, font=("Segoe UI", 12), width=5, anchor='c')
-        #live_recovery_label.grid(row=0, column=col, sticky =S+W)
-        live_recovery_label.place(rely=0.0, relx=0.0, x=4, y=4, anchor=NW)
+        live_recovery_label = tkinter.Label(parent, textvariable=live_recovery_var, font=("Segoe UI", 12), width=5, anchor='c')
+        live_recovery_label.place(rely=0.0, relx=0.0, x=4, y=4, anchor=tkinter.NW)
         return live_recovery_var
 
     def create_frame_advantage_label(self, col):
-        frame_advantage_var = StringVar()
+        frame_advantage_var = tkinter.StringVar()
         frame_advantage_var.set('?')
-        frame_advantage_label = Label(self.toplevel, textvariable=frame_advantage_var, font=("Consolas", 44), width=4, anchor='c',
+        frame_advantage_label = tkinter.Label(self.toplevel, textvariable=frame_advantage_var, font=("Consolas", 44), width=4, anchor='c',
                                         borderwidth=1, relief='ridge')
         frame_advantage_label.grid(row=0, column=col)
         return frame_advantage_var, frame_advantage_label
 
     def create_attack_type_label(self, col):
-        attack_type_var = StringVar()
+        attack_type_var = tkinter.StringVar()
         attack_type_var.set('?')
-        attack_type_label = Label(self.toplevel, textvariable=attack_type_var, font=("Verdana", 12), width=10, anchor='c',
+        attack_type_label = tkinter.Label(self.toplevel, textvariable=attack_type_var, font=("Verdana", 12), width=10, anchor='c',
                                     borderwidth=4, relief='ridge')
         attack_type_label.grid(row=1, column=col)
         return attack_type_var
 
     def create_textbox(self, col):
-        textbox = Text(self.toplevel, font=("Consolas", 11), wrap=NONE, highlightthickness=0, pady=0, relief='flat')
-        textbox.grid(row=0, column=col, rowspan=2, sticky=N + S + W + E)
+        textbox = tkinter.Text(self.toplevel, font=("Consolas", 11), wrap=tkinter.NONE, highlightthickness=0, pady=0, relief='flat')
+        textbox.grid(row=0, column=col, rowspan=2, sticky=tkinter.N + tkinter.S + tkinter.W + tkinter.E)
         textbox.configure(background=self.background_color)
-        textbox.configure(foreground=CurrentColorScheme.scheme[ColorSchemeEnum.system_text])
+        textbox.configure(foreground=Overlay.CurrentColorScheme.scheme[Overlay.ColorSchemeEnum.system_text])
         return textbox
-
 
     def update_state(self):
         if self.show_live_framedata:
