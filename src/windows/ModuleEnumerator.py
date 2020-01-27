e263ca392f3ea04b7dbc76ead3bdebf44c61dc1b
dcep93 <dan@quizlet.com>
2020-01-26 17:45:55 -0800

    moduleenum

--- src/windows/ModuleEnumerator.py
+++ src/windows/ModuleEnumerator.py
@@ -2,81 +2,46 @@ import sys
 
 import windows
 
+class MODULEENTRY32(windows.ctypes.Structure):
+    _fields_ = [( 'dwSize' , windows.ctypes.wintypes.DWORD ) ,
+                ( 'th32ModuleID' , windows.ctypes.wintypes.DWORD ),
+                ( 'th32ProcessID' , windows.ctypes.wintypes.DWORD ),
+                ( 'GlblcntUsage' , windows.ctypes.wintypes.DWORD ),
+                ( 'ProccntUsage' , windows.ctypes.wintypes.DWORD ) ,
+                ( 'modBaseAddr' , windows.ctypes.POINTER(windows.ctypes.wintypes.BYTE) ) ,
+                ( 'modBaseSize' , windows.ctypes.wintypes.DWORD ) ,
+                ( 'hModule' , windows.ctypes.wintypes.HMODULE ) ,
+                ( 'szModule' , windows.ctypes.c_char * 256 ),
+                ( 'szExePath' , windows.ctypes.c_char * 260 ) ]
+
 def GetModuleAddressByPIDandName(pid, name):
-    DWORD = windows.ctypes.wintypes.DWORD
-    class MODULEENTRY32(windows.ctypes.Structure):
-        _fields_ = [( 'dwSize' , DWORD ) ,
-                    ( 'th32ModuleID' , DWORD ),
-                    ( 'th32ProcessID' , DWORD ),
-                    ( 'GlblcntUsage' , DWORD ),
-                    ( 'ProccntUsage' , DWORD ) ,
-                    ( 'modBaseAddr' , windows.ctypes.POINTER(windows.ctypes.wintypes.BYTE) ) ,
-                    ( 'modBaseSize' , DWORD ) ,
-                    ( 'hModule' , windows.ctypes.wintypes.HMODULE ) ,
-                    ( 'szModule' , windows.ctypes.c_char * 256 ),
-                    ( 'szExePath' , windows.ctypes.c_char * 260 ) ]
-    # const variable
     # Establish rights and basic options needed for all process declartion / iteration
-    TH32CS_SNAPPROCESS = 2
     STANDARD_RIGHTS_REQUIRED = 0x000F0000
     SYNCHRONIZE = 0x00100000
     PROCESS_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | SYNCHRONIZE | 0xFFF)
     TH32CS_SNAPMODULE = 0x00000008
-    TH32CS_SNAPTHREAD = 0x00000004
 
+    me32 = MODULEENTRY32()
+    me32.dwSize = windows.ctypes.sizeof(MODULEENTRY32)
 
     windll = windows.ctypes.windll
-    CreateToolhelp32Snapshot= windll.kernel32.CreateToolhelp32Snapshot
-    Process32First = windll.kernel32.Process32First
-    Process32Next = windll.kernel32.Process32Next
-    Module32First = windll.kernel32.Module32First
-    Module32Next = windll.kernel32.Module32Next
-    GetLastError = windll.kernel32.GetLastError
-    OpenProcess = windll.kernel32.OpenProcess
-    GetPriorityClass = windll.kernel32.GetPriorityClass
-    CloseHandle = windll.kernel32.CloseHandle
 
+    hModuleSnap = windll.kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid)
+    if hModuleSnap == -1:
+        print('CreateToolhelp32Snapshot Error [%d]' % windows.w.GetLastError())
+        print('Build the code yourself? This is the error for using 32-bit Python. Try the 64-bit version.')
 
-    try:
-        addressToReturn = None
+    ret = windll.kernel32.Module32First(hModuleSnap, windows.ctypes.pointer(me32) )
+    if ret is 0:
+        print('ListProcessModules() Error on Module32First[%d]' % windows.w.GetLastError())
+        windows.w.CloseHandle(hModuleSnap)
 
-        ProcessID=pid
-        hModuleSnap = DWORD
-        me32 = MODULEENTRY32()
-        me32.dwSize = windows.ctypes.sizeof( MODULEENTRY32 )
-        #me32.dwSize = 5000
+    addressToReturn = None
+    while ret:
+        if name == me32.szModule.decode("utf-8"):
+            addressToReturn = me32.hModule
 
-        hModuleSnap = CreateToolhelp32Snapshot( TH32CS_SNAPMODULE, ProcessID )
-        if hModuleSnap == -1:
-            print('CreateToolhelp32Snapshot Error [%d]' % GetLastError())
-            print('Build the code yourself? This is the error for using 32-bit Python. Try the 64-bit version.')
+        ret = windll.kernel32.Module32Next(hModuleSnap, windows.ctypes.pointer(me32))
+    windows.w.CloseHandle(hModuleSnap)
 
-        ret = Module32First( hModuleSnap, windows.ctypes.pointer(me32) )
-        if ret == 0 :
-            print('ListProcessModules() Error on Module32First[%d]' % GetLastError())
-            CloseHandle( hModuleSnap )
-        PROGMainBase = False
-        while ret :
-            #print(me32.dwSize)
-            #print(me32.th32ModuleID)
-            #print(me32.th32ProcessID)
-            #print(me32.GlblcntUsage)
-            #print(me32.ProccntUsage)
-            #print(me32.modBaseAddr)
-            #print(me32.modBaseSize)
-            #print(me32.hModule)
-            #print(me32.szModule)
-            #print(me32.szExePath)
-            #print(name == me32.szModule.decode("utf-8"))
-            if name == me32.szModule.decode("utf-8"):
-                addressToReturn = me32.hModule
-                #print(me32.modBaseAddr.value)
-
-            ret = Module32Next( hModuleSnap , windows.ctypes.pointer(me32) )
-        CloseHandle( hModuleSnap )
-
-        return addressToReturn
-
-    except:
-        print("Error in ListProcessModules")
-        raise
+    return addressToReturn
