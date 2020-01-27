76bdc58feadd17e0c4a55ab07d1d242f600944da
dcep93 <dan@quizlet.com>
2020-01-27 14:01:58 -0800

    word break too

--- find_unused.sh
+++ find_unused.sh
@@ -6,15 +6,15 @@ raw_files=`find src -name '*.py'`
 
 function count_matches() {
     func="$1"
-    grep "$func" $raw_files | wc -l
+    grep "\b$func\b" $raw_files | wc -l
 }
 
 for raw_file in $raw_files; do
     file=$(basename $raw_file)
-    for func in `grep -o 'def [A-Za-z]\+' $raw_file | awk '{print $2}'`; do
-        count=`count_matches $func`
+    for func in `grep -o 'def [A-Za-z_0-9]\+' $raw_file | awk '{print $2}'`; do
+        count=`count_matches $func` || (echo $func && exit 1)
         if [[ $count -lt 2 ]]; then
-            echo "$file.$func"
+            echo "$file $func"
         fi
     done
 done
