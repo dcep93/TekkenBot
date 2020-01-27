2daf1a0106bc42e3fd56c509e53718bf66c0b2c7
dcep93 <dan@quizlet.com>
2020-01-27 13:55:09 -0800

    fix find unused

--- find_unused.sh
+++ find_unused.sh
@@ -2,17 +2,19 @@
 
 set -euo pipefail
 
+raw_files=`find src -name '*.py'`
+
 function count_matches() {
     func="$1"
-    grep -r "$func" src | wc -l
+    grep "$func" $raw_files | wc -l
 }
 
-for raw_file in `find src -name '*.py'`; do
+for raw_file in $raw_files; do
     file=$(basename $raw_file)
     for func in `grep -o 'def [A-Za-z]\+' $raw_file | awk '{print $2}'`; do
         count=`count_matches $func`
         if [[ $count -lt 2 ]]; then
-            echo "$file.$func" && exit 1
+            echo "$file.$func"
         fi
     done
 done
