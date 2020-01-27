df2916be95edd5e3f39bc1f766f5ffa281d5b06c
dcep93 <dan@quizlet.com>
2020-01-26 17:00:41 -0800

    cio

--- src/gui/CommandInputOverlay.py
+++ src/gui/CommandInputOverlay.py
@@ -1,5 +1,6 @@
 from . import Overlay
 from . import t_tkinter
+
 from game_parser.MoveInfoEnums import InputDirectionCodes
 
 symbol_map = {
@@ -17,6 +18,8 @@ symbol_map = {
 
 class CommandInputOverlay(Overlay.Overlay):
     length = 60
+    input_tag = "inputs"
+
     def __init__(self, master, state):
         super().__init__(master, (1200, 86))
 
@@ -38,44 +41,39 @@ class CommandInputOverlay(Overlay.Overlay):
 
     def update_state(self):
         last_state = self.state.stateLog[-1]
-        if last_state.is_player_player_one:
-            player = last_state.bot
-        else:
-            player = last_state.opp
+        player = last_state.bot if last_state.is_player_player_one else last_state.opp
         
         input_state = player.GetInputState()
         color = self.color_from_cancel_booleans(player)
         self.update_input(input_state, color)
 
-    def color_from_cancel_booleans(self, obj):
-        if obj.is_parry_1:
+    def color_from_cancel_booleans(self, player):
+        if player.is_parry_1:
             fill_color = 'orange'
-        elif obj.is_parry_2:
+        elif player.is_parry_2:
             fill_color = 'yellow'
-        elif obj.is_bufferable:
+        elif player.is_bufferable:
             fill_color = 'MediumOrchid1'
-        elif obj.is_cancelable:
+        elif player.is_cancelable:
             fill_color = 'SteelBlue1'
         else:
             fill_color = 'firebrick1'
         return fill_color
 
     def update_input(self, input_state, cancel_color):
-        input_tag = "inputs"
         self.stored_inputs.append((input_state,cancel_color))
         if len(self.stored_inputs) >= self.length:
             self.stored_inputs = self.stored_inputs[-self.length:]
             if input_state != self.stored_inputs[-2]:
-                self.canvas.delete(input_tag)
-                for i, stored_input_and_cancel in enumerate(self.stored_inputs):
-                    stored_input, stored_cancel = stored_input_and_cancel
-                    self.update_canvas_with_input(i, stored_input, stored_cancel, input_tag)
+                self.canvas.delete(self.input_tag)
+                for i, (stored_input, stored_cancel) in enumerate(self.stored_inputs):
+                    self.update_canvas_with_input(i, stored_input, stored_cancel)
 
-    def update_canvas_with_input(self, i, stored_input, stored_cancel, input_tag):
+    def update_canvas_with_input(self, i, stored_input, stored_cancel):
         direction_code, attack_code, rage_flag = stored_input
         posn = i * self.step + (self.step / 2)
-        self.canvas.create_text(posn, 30, text=symbol_map[direction_code], fill='snow',  font=("Consolas", 20), tag=input_tag)
+        self.canvas.create_text(posn, 30, text=symbol_map[direction_code], fill='snow', font=("Consolas", 20), tag=self.input_tag)
         self.canvas.create_text(posn, 55, text=attack_code.name.replace('x', '').replace('N', ''), fill='snow',  font=("Consolas", 12), tag=input_tag)
         x0 = i * self.step + 4
         x1 = x0 + self.step - 8
-        self.canvas.create_rectangle(x0, 70, x1, self.h - 5, fill=stored_cancel, tag=input_tag)
+        self.canvas.create_rectangle(x0, 70, x1, self.h - 5, fill=stored_cancel, tag=self.input_tag)
