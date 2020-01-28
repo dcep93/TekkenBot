12392bf02abd2684813ebbf55208453c140b75f4
dcep93 <dan@quizlet.com>
2020-01-28 12:20:26 -0800

    find unused

--- find_unused.sh
+++ find_unused.sh
@@ -9,14 +9,21 @@ function count_matches() {
     grep "\b$func\b" $raw_files | wc -l
 }
 
-for raw_file in $raw_files; do
+function find_unused() {
+    raw_file="$1"
     file=$(basename $raw_file)
-    if [[ "$file" == "GameStateGetters.py" ]]; then continue; fi
     for func in `grep -o 'def [A-Za-z_0-9]\+' $raw_file | awk '{print $2}'`; do
         if [[ "$file" == "TekkenBotPrime.py" && "$func" == "flush" ]]; then continue; fi
+        if [[ "$file" == "MovelistParser.py" && "$func" == "__getstate__" ]]; then continue; fi
         count=`count_matches $func` || (echo $func && exit 1)
         if [[ $count -lt 2 ]]; then
             echo "$file $func"
+        else
+            echo $func keep
         fi
     done
+}
+
+for raw_file in $raw_files;
+    find_unused raw_file
 done
