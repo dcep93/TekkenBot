c8b5805649d9718800d6294a02f0a8365b533fd8
dcep93 <dan@quizlet.com>
2020-01-29 11:39:05 -0800

    move enums

--- src/game_parser/MovelistParser.py
+++ src/game_parser/MovelistParser.py
@@ -2,11 +2,13 @@ import enum
 import collections
 import struct
 
+from . import MoveInfoEnums
+
 class MoveNode:
     def __init__(self, forty_bytes, offset, all_bytes, all_names):
         unpacked = struct.unpack('<H', forty_bytes[0:2])[0]
         try:
-            self.direction_bytes = MovelistInputCodes(unpacked).name
+            self.direction_bytes = MoveInfoEnums.MovelistInputCodes(unpacked).name
         except:
             self.direction_bytes = '{:x}'.format(unpacked)
 
@@ -14,13 +16,13 @@ class MoveNode:
 
         attack_bytes = struct.unpack('<H', forty_bytes[4:6])[0]
         try:
-            self.attack_bytes = MovelistButtonCodes(attack_bytes).name
+            self.attack_bytes = MoveInfoEnums.MovelistButtonCodes(attack_bytes).name
         except:
             self.attack_bytes = str(attack_bytes)
 
         button_press = struct.unpack('<H', forty_bytes[6:8])[0]
         try:
-            self.button_press = ButtonPressCodes(button_press).name
+            self.button_press = MoveInfoEnums.ButtonPressCodes(button_press).name
         except:
             self.button_press = str(button_press)
 
@@ -36,7 +38,7 @@ class MoveNode:
 
         active = struct.unpack('<B', forty_bytes[38:39])[0]
         try:
-            self.move_requires_input = ActiveCodes(active).name
+            self.move_requires_input = MoveInfoEnums.ActiveCodes(active).name
         except:
             self.move_requires_input = str(active)
 
@@ -109,18 +111,18 @@ class MovelistParser:
 
         sort_presses = collections.defaultdict(int)
 
-        sort_directions[MovelistInputCodes.FC.name] = 110
-        sort_directions[MovelistInputCodes.N.name] = 100
-        sort_directions[MovelistInputCodes.ws.name] = 90
-        sort_directions[MovelistInputCodes.uf.name] = 80
+        sort_directions[MoveInfoEnums.MovelistInputCodes.FC.name] = 110
+        sort_directions[MoveInfoEnums.MovelistInputCodes.N.name] = 100
+        sort_directions[MoveInfoEnums.MovelistInputCodes.ws.name] = 90
+        sort_directions[MoveInfoEnums.MovelistInputCodes.uf.name] = 80
 
-        sort_attacks[MovelistButtonCodes.B_1.name] = 100
-        sort_attacks[MovelistButtonCodes.B_2.name] = 99
-        sort_attacks[MovelistButtonCodes.B_3.name] = 98
-        sort_attacks[MovelistButtonCodes.B_4.name] = 97
+        sort_attacks[MoveInfoEnums.MovelistButtonCodes.B_1.name] = 100
+        sort_attacks[MoveInfoEnums.MovelistButtonCodes.B_2.name] = 99
+        sort_attacks[MoveInfoEnums.MovelistButtonCodes.B_3.name] = 98
+        sort_attacks[MoveInfoEnums.MovelistButtonCodes.B_4.name] = 97
 
-        sort_presses[ButtonPressCodes.Press.name] = 100
-        sort_presses[ButtonPressCodes.NULL.name] = -2
+        sort_presses[MoveInfoEnums.ButtonPressCodes.Press.name] = 100
+        sort_presses[MoveInfoEnums.ButtonPressCodes.NULL.name] = -2
 
         self.move_id_to_input = {}
         for move_id, value in self.democratically_chosen_input.items():
@@ -155,7 +157,7 @@ class MovelistParser:
 
             tuple = self.move_id_to_input[move_id]
 
-            if not tuple[0] in (MovelistInputCodes.NULL.name, MovelistInputCodes.N.name):
+            if not tuple[0] in (MoveInfoEnums.MovelistInputCodes.NULL.name, MoveInfoEnums.MovelistInputCodes.N.name):
 
                 if move_id > -1 and move_id < len(self.names) and '66' in self.names[move_id] and not '666' in self.names[move_id]:# and ('66' in self.names[previous_move_id] or 'DASH' in self.names[previous_move_id]):
                     input += 'ff'
@@ -165,7 +167,7 @@ class MovelistParser:
             if 'Release' in tuple[2]:
                 input += '*'
 
-            if not tuple[1] in (MovelistButtonCodes.NULL.name,):
+            if not tuple[1] in (MoveInfoEnums.MovelistButtonCodes.NULL.name,):
                 input += tuple[1].replace("B_", "").replace("_PLUS_", "+")
 
             if previous_move_id >= 0 and previous_move_id < len(self.names) and move_id >= 0 and move_id < len(self.names):
@@ -181,155 +183,3 @@ class MovelistParser:
         state = self.__dict__.copy()
         state['bytes'] = state['bytes'].value
         return state
-
-class ButtonPressCodes(enum.Enum):
-    NULL = 0
-    Release_4 = 4
-    Release_8 = 8
-    UNK_20 = 0x20
-    Release = 0x2000
-    Press = 0x4000
-
-class ActiveCodes(enum.Enum):
-    NULL = 0
-    A = 65 #active? seem to require a button to go into cancel
-    P = 80 #passive? seem to happen without further input
-
-class MovelistButtonCodes(enum.Enum):
-    NULL = 0
-    B_1 = 1
-    B_2 = 2
-    B_1_PLUS_2 = 3
-    B_3 = 4
-    B_1_PLUS_3 = 5
-    B_2_PLUS_3 = 6
-    B_1_PLUS_2_PLUS_3 = 7
-    B_4 = 8
-    B_1_PLUS_4 = 9
-    B_2_PLUS_4 = 10
-    B_1_PLUS_2_PLUS_4 = 11
-    B_3_PLUS_4 = 12
-    B_1_PLUS_3_PLUS_4 = 13
-    B_2_PLUS_3_PLUS_4 = 14
-    B_1_PLUS_2_PLUS_3_PLUS_4 = 15
-    B_R = 16
-
-    UNK_600= 0x600 #1+2 only maybe? on hwoarangs b2, not HOLD
-
-
-class MovelistInputCodes(enum.Enum):
-    NULL = 0
-
-    N = 0x20
-
-    u = 0x100
-    ub = 0x80
-    uf = 0x200
-
-    f = 0x40
-    b = 0x10
-
-    d = 4
-    df = 8
-    db = 2
-
-
-    #the following codes exist only in the movelist, not in player data
-    FC = 6
-    d_df = 0xc #down or down forward but not down back
-    _d = 0xe #sometimes d as well??
-    UNK_12 = 0x12
-    UNK_2e = 0x2e #guard?
-    UNK_48 = 0x48 #crouch turn while holding down?
-    UNK_5e = 0x5e #pitcher glove cancel, B after U+2+3 in TTT2
-    UNK_60 = 0x60
-    RUNx = 0x70 #actually BT??? sometimes running???
-    _ub = 0x90 # item command? u/b+1 for king
-    UNK_92 = 0x92 # possible alternate u/b input?
-    UNK_104 = 0x104 #item command?
-    FACE_DOWN = 0x120
-    UNK_120 = 0x120 #leads to jump
-    UNK_248 = 0x248 #???
-    UNK_380 = 0x380 #Vp_sJUMPr00 roll jump?
-    UNK_38a = 0x38a
-    UNK_3ae = 0x3ae
-    UNK_3c0 = 0x3c0 #all lead back to standing
-    UNK_3de = 0x3de
-    UNK_3ec = 0x3ec
-    UNK_3ee = 0x3ee #Eliza's sleep cancel, so like, NOT holding b
-    ws = 0x3f0 #not standing backturn?
-    UNK_402 = 0x402
-    UNK_404 = 0x404
-    UNK_408 = 0x408
-    UNK_40e = 0x40e
-
-
-
-    _Q = 0x8000 #??? lots of these
-    ff = 0x8001
-    bb = 0x8002
-    UNK_8003 = 0x8003 #sidesteps?
-    UNK_8004 = 0x8004  # sidesteps?
-    UNK_800b = 0x800b #hit standing? block standing?
-    UNK_800c = 0x800c #only exists on move_id=0?
-
-    db_f_34 = 0x8018  #guard #King's tombstone
-    UNK_8019 = 0x8019  # guard
-    UNK_801a = 0x801a  # guard
-    UNK_801b = 0x801b  # guard
-
-    UNK_803a = 0x803a  # standing
-
-    RUN_CHOP = 0x80ac  # run chop
-    RUN_KICK = 0x80ae  # run chop
-
-    UNK_80af = 0x80af  # guard
-
-    RUN_1 = 0x80b0  # run lp?
-    RUN_2 = 0x80b1  # run rp?
-    RUN_3 = 0x80b2  # run lk?
-    RUN_4 = 0x80b3  # run rk?
-
-    #qcf states for eliza, all the ways to make a qcf, maybe storing the input
-    qcf_fb = 0x80fb #qcf+1 # this b-f for Kazumi
-    qcf_fc = 0x80fc #qcf+2
-    qcf_fd = 0x80fd #qcf+1
-    qcf_fe = 0x80fe #qcf+2
-    qcf_ff = 0x80ff  #EX only
-    qcf_100 = 0x8100  # EX only
-    qcf_101 = 0x8101  # No fireball?
-    qcf_102 = 0x8102  # No fireball?
-    qcf_103 = 0x8103  # super (double qcf)
-    qcf_104 = 0x8104  # super (double qcf)
-
-    #dp states
-    dp_0b = 0x8010b #EX
-    dp_0c = 0x8010c  # EX
-    dp_0d = 0x8010d  # 1
-    dp_0e = 0x8010e  # 2
-    dp_0f = 0x8010f  # 1
-    dp_10 = 0x80110  # 2
-
-    #qcb states
-    qcb_11 = 0x8011
-    qcb_12 = 0x8012
-    qcb_13 = 0x8013
-    qcb_14 = 0x8014
-    qcb_15 = 0x8015
-    qcb_16 = 0x8016
-    qcb_17 = 0x8017
-    qcb_18 = 0x8018
-    qcb_19 = 0x8019
-    qcb_1a = 0x801a
-    #Missing?
-    qcb_1c = 0x801c
-    qcb_1d = 0x801d
-
-    f_qcf_12 = 0x8031 #gigas command throw
-
-    EX_CANCEL_1 = 0x8122
-    EX_CANCEL_2 = 0x8123
-
-    qcf_129 = 0x8129 #1, seems to be the most common, maybe the 'normal' qcf
-    qcf_12a = 0x812a #2
-    qcf_12e = 0x812e
