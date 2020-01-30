24193e44452ca22b1a9f0ce2f35f77147ab41b2d
dcep93 <dan@quizlet.com>
2020-01-29 16:09:14 -0800

    remove some attrs

--- src/game_parser/MovelistParser.py
+++ src/game_parser/MovelistParser.py
@@ -5,7 +5,7 @@ import struct
 from . import MoveInfoEnums
 
 class MoveNode:
-    def __init__(self, forty_bytes, offset, all_bytes, all_names):
+    def __init__(self, forty_bytes, offset, movelist_bytes, all_names):
         unpacked = struct.unpack('<H', forty_bytes[0:2])[0]
         try:
             self.direction_bytes = MoveInfoEnums.MovelistInputCodes(unpacked).name
@@ -26,10 +26,10 @@ class MoveNode:
         except:
             self.button_press = str(button_press)
 
-        self.pointer_one = struct.unpack('<Q', forty_bytes[8:16])[0] - offset
-        self.pointer_two = struct.unpack('<Q', forty_bytes[16:24])[0] - offset
-        self.number_one = struct.unpack('<I', all_bytes[self.pointer_one: self.pointer_one + 4])[0]
-        self.number_two = struct.unpack('<I', all_bytes[self.pointer_two: self.pointer_two + 4])[0]
+        pointer_one = struct.unpack('<Q', forty_bytes[8:16])[0] - offset
+        pointer_two = struct.unpack('<Q', forty_bytes[16:24])[0] - offset
+        self.number_one = struct.unpack('<I', movelist_bytes[pointer_one: pointer_one + 4])[0]
+        self.number_two = struct.unpack('<I', movelist_bytes[pointer_two: pointer_two + 4])[0]
 
         self.unknown_bool = struct.unpack('<I', forty_bytes[24:28])[0]
         self.cancel_window_1 = struct.unpack('<I', forty_bytes[28:32])[0]
@@ -54,35 +54,30 @@ class MoveNode:
 
 class MovelistParser:
     def __init__(self, movelist_bytes, movelist_pointer):
-        self.pointer = movelist_pointer
-        self.parse_header(movelist_bytes)
-
-    def parse_header(self, all_bytes):
         header_length = 0x2e8
-        header_bytes = all_bytes[0:header_length]
-        identifier = self.header_line(0, all_bytes)
-        char_name_address = self.header_line(1, all_bytes)
-        developer_name_address = self.header_line(2, all_bytes)
-        date_address = self.header_line(3, all_bytes)
-        timestamp_address = self.header_line(4, all_bytes)
+        header_bytes = movelist_bytes[0:header_length]
+        identifier = self.header_line(0, movelist_bytes, movelist_pointer)
+        char_name_address = self.header_line(1, movelist_bytes, movelist_pointer)
+        developer_name_address = self.header_line(2, movelist_bytes, movelist_pointer)
+        date_address = self.header_line(3, movelist_bytes, movelist_pointer)
+        timestamp_address = self.header_line(4, movelist_bytes, movelist_pointer)
 
-        self.char_name = all_bytes[char_name_address:developer_name_address].strip(b'\00').decode('utf-8')
+        self.char_name = movelist_bytes[char_name_address:developer_name_address].strip(b'\00').decode('utf-8')
         print("Parsing movelist for {}".format(self.char_name))
 
         unknown_regions = {}
         for i in range(42, 91, 2):
-            unknown_regions[i] = self.header_line(i, all_bytes)
+            unknown_regions[i] = self.header_line(i, movelist_bytes, movelist_pointer)
 
-        self.names_double = all_bytes[header_length:unknown_regions[42]].split(b'\00')[4:]
+        self.names_double = movelist_bytes[header_length:unknown_regions[42]].split(b'\00')[4:]
         self.names = []
         for i in range(0, len(self.names_double) - 1, 2):
             self.names.append(self.names_double[i].decode('utf-8'))
 
-
-        self.move_nodes_raw = all_bytes[unknown_regions[54]:unknown_regions[58]] #there's two regions of move nodes, first one might be blocks????
+        self.move_nodes_raw = movelist_bytes[unknown_regions[54]:unknown_regions[58]] #there's two regions of move nodes, first one might be blocks????
         self.move_nodes = []
         for i in range(0, len(self.move_nodes_raw), 40):
-            self.move_nodes.append(MoveNode(self.move_nodes_raw[i:i+40], self.pointer, all_bytes, self.names))
+            self.move_nodes.append(MoveNode(self.move_nodes_raw[i:i+40], movelist_pointer, movelist_bytes, self.names))
 
         self.can_move_be_done_from_neutral = {}
 
@@ -93,13 +88,13 @@ class MovelistParser:
             if node.cancel_window_1 >= 0x7FFF:
                 self.can_move_be_done_from_neutral[move_id] = True
 
-        self.democratically_chosen_input = {}
+        democratically_chosen_input = {}
         for node in self.move_nodes:
-            if not node.move_id in self.democratically_chosen_input:
+            if not node.move_id in democratically_chosen_input:
                 inputs = []
-                self.democratically_chosen_input[node.move_id] = inputs
+                democratically_chosen_input[node.move_id] = inputs
             else:
-                inputs = self.democratically_chosen_input[node.move_id]
+                inputs = democratically_chosen_input[node.move_id]
             inputs.append((node.direction_bytes, node.attack_bytes, node.button_press))
 
         sort_directions = collections.defaultdict(int)
@@ -122,7 +117,7 @@ class MovelistParser:
         sort_presses[MoveInfoEnums.ButtonPressCodes.NULL.name] = -2
 
         self.move_id_to_input = {}
-        for move_id, value in self.democratically_chosen_input.items():
+        for move_id, value in democratically_chosen_input.items():
 
             candidates = value
 
@@ -135,11 +130,11 @@ class MovelistParser:
 
                 self.move_id_to_input[move_id] = (direction, button, press)
 
-        self.movelist_names = all_bytes[0x2E8:200000].split(b'\00') # Todo: figure out the actual size of the name movelist
+        self.movelist_names = movelist_bytes[0x2E8:200000].split(b'\00') # Todo: figure out the actual size of the name movelist
 
-    def header_line(self, line, all_bytes):
-        header_bytes = all_bytes[line * 8:(line+1) * 8]
-        return struct.unpack('<Q', header_bytes)[0] - self.pointer
+    def header_line(self, line, movelist_bytes, movelist_pointer):
+        header_bytes = movelist_bytes[line * 8:(line+1) * 8]
+        return struct.unpack('<Q', header_bytes)[0] - movelist_pointer
 
     def can_be_done_from_neutral(self, move_id):
         if move_id in self.can_move_be_done_from_neutral:
