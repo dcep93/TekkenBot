ed9c6fde5db566f5b2075eae75f6257739b7d94c
dcep93 <dan@quizlet.com>
2020-01-30 17:35:55 -0800

    remove attrs

--- src/game_parser/MovelistParser.py
+++ src/game_parser/MovelistParser.py
@@ -64,19 +64,19 @@ class MovelistParser:
         for i in range(42, 91, 2):
             unknown_regions[i] = self.header_line(i, movelist_bytes, movelist_pointer)
 
-        self.names_double = movelist_bytes[header_length:unknown_regions[42]].split(b'\00')[4:]
+        names_double = movelist_bytes[header_length:unknown_regions[42]].split(b'\00')[4:]
         self.names = []
-        for i in range(0, len(self.names_double) - 1, 2):
-            self.names.append(self.names_double[i].decode('utf-8'))
+        for i in range(0, len(names_double) - 1, 2):
+            self.names.append(names_double[i].decode('utf-8'))
 
-        self.move_nodes_raw = movelist_bytes[unknown_regions[54]:unknown_regions[58]] #there's two regions of move nodes, first one might be blocks????
-        self.move_nodes = []
-        for i in range(0, len(self.move_nodes_raw), 40):
-            self.move_nodes.append(MoveNode(self.move_nodes_raw[i:i+40], movelist_pointer, movelist_bytes, self.names))
+        move_nodes_raw = movelist_bytes[unknown_regions[54]:unknown_regions[58]] #there's two regions of move nodes, first one might be blocks????
+        move_nodes = []
+        for i in range(0, len(move_nodes_raw), 40):
+            move_nodes.append(MoveNode(move_nodes_raw[i:i+40], movelist_pointer, movelist_bytes, self.names))
 
         self.can_move_be_done_from_neutral = {}
 
-        for node in self.move_nodes:
+        for node in move_nodes:
             move_id = node.move_id
             if not move_id in self.can_move_be_done_from_neutral:
                 self.can_move_be_done_from_neutral[move_id] = False
@@ -84,7 +84,7 @@ class MovelistParser:
                 self.can_move_be_done_from_neutral[move_id] = True
 
         democratically_chosen_input = {}
-        for node in self.move_nodes:
+        for node in move_nodes:
             if not node.move_id in democratically_chosen_input:
                 inputs = []
                 democratically_chosen_input[node.move_id] = inputs
@@ -127,7 +127,8 @@ class MovelistParser:
 
         self.movelist_names = movelist_bytes[0x2E8:200000].split(b'\00') # Todo: figure out the actual size of the name movelist
 
-    def header_line(self, line, movelist_bytes, movelist_pointer):
+    @staticmethod
+    def header_line(line, movelist_bytes, movelist_pointer):
         header_bytes = movelist_bytes[line * 8:(line+1) * 8]
         return struct.unpack('<Q', header_bytes)[0] - movelist_pointer
 
@@ -141,29 +142,29 @@ class MovelistParser:
         empty_cancel_strings =[ 'b', '_B', '_R_D', 'y', 'Rv', '_R', '_D', 'Y']
 
         if move_id in self.move_id_to_input:
-            input = ''
+            string = ''
             last_move_was_empty_cancel = False
 
-            tuple = self.move_id_to_input[move_id]
+            input_tuple = self.move_id_to_input[move_id]
 
-            if not tuple[0] in (MoveInfoEnums.MovelistInputCodes.NULL.name, MoveInfoEnums.MovelistInputCodes.N.name):
+            if not input_tuple[0] in (MoveInfoEnums.MovelistInputCodes.NULL.name, MoveInfoEnums.MovelistInputCodes.N.name):
 
-                if move_id > -1 and move_id < len(self.names) and '66' in self.names[move_id] and not '666' in self.names[move_id]:# and ('66' in self.names[previous_move_id] or 'DASH' in self.names[previous_move_id]):
-                    input += 'ff'
+                if move_id > -1 and move_id < len(self.names) and '66' in self.names[move_id] and not '666' in self.names[move_id]:
+                    string += 'ff'
                 else:
-                    input += tuple[0]
+                    string += input_tuple[0]
 
-            if 'Release' in tuple[2]:
-                input += '*'
+            if 'Release' in input_tuple[2]:
+                string += '*'
 
-            if not tuple[1] in (MoveInfoEnums.MovelistButtonCodes.NULL.name,):
-                input += tuple[1].replace("B_", "").replace("_PLUS_", "+")
+            if not input_tuple[1] in (MoveInfoEnums.MovelistButtonCodes.NULL.name,):
+                string += input_tuple[1].replace("B_", "").replace("_PLUS_", "+")
 
             if previous_move_id >= 0 and previous_move_id < len(self.names) and move_id >= 0 and move_id < len(self.names):
 
                 if self.names[previous_move_id] in ([self.names[move_id] + s for s in empty_cancel_strings]):
                     last_move_was_empty_cancel = True
 
-            return input, last_move_was_empty_cancel
+            return string, last_move_was_empty_cancel
         else:
             return "N/A", False
