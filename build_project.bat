f8d363a87f669c95b2c92089d9f2fd652f8bb20e
unknown <dcep93@gmail.com>
2020-02-24 19:10:20 -0800

    woo we can make an exe

--- build_project.bat
+++ build_project.bat
@@ -1,3 +1,4 @@
 ::get pyinstaller if you haven't already!
 ::pip install pyinstaller
-pyinstaller --noconfirm --clean --icon=src/export/assets/tekken_bot_close.ico --add-data src/export;export --name TekkenBotPrime src/main.py
+pyinstaller --noconfirm --onefile --console --clean --icon=src/export/assets/tekken_bot_close.ico --name TekkenBotPrime src/main.py
+cp -r src/export dist/export
--- src/misc/Path.py
+++ src/misc/Path.py
@@ -1,6 +1,9 @@
 import os
+import sys
 
 __path__ = '%s/' % os.path.dirname(os.path.dirname(__file__))
+print(__path__)
+__path__ = os.path.dirname(sys.argv[0])
 
 def path(rel_path):
     return os.path.join(__path__, rel_path)
