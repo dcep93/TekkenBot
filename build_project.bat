597e52dcf62a81754aa99e336fb8b5d14740bd50
dcep93 <dan@quizlet.com>
2020-02-25 14:39:09 -0800

    build

--- build_project.bat
+++ build_project.bat
@@ -1,4 +1,4 @@
 ::get pyinstaller if you haven't already!
 ::pip install pyinstaller
-pyinstaller --noconfirm --onefile --windowed --clean --icon=src/export/assets/tekken_bot_close.ico --name TekkenBotPrime src/main.py
+pyinstaller --noconfirm --onefile --windowed --clean --icon=asssets/img/tekken_bot_close.ico --name TekkenBotPrime main.py
 cp dist/TekkenBotPrime.exe ./
