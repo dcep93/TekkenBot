ecd8e714b3920fadaf894a789f1297bcb5254a38
unknown <dcep93@gmail.com>
2020-03-08 13:32:44 -0700

    no underscore

--- src/record/Record.py
+++ src/record/Record.py
@@ -126,7 +126,7 @@ def get_raw_move(input_state):
     direction_code, attack_code, _ = input_state
     direction_string = direction_code.name
     attack_string = attack_code.name.replace('x', '').replace('N', '')
-    return '%s_%s' % (direction_string, attack_string)
+    return '%s%s' % (direction_string, attack_string)
 
 def loads_moves(compacted_moves):
     moves = []
@@ -148,8 +148,8 @@ def move_to_hexes(move, reverse, p1=True):
         p1_codes = move_to_hexes(p1_move, reverse, True)
         p2_codes = move_to_hexes(p2_move, reverse, False)
         return p1_codes + p2_codes
-    parts = move.split('_')
-    direction_string, attack_string = parts
+    direction_string = ''.join([i for i in move if i not in '1234'])
+    attack_string = move[len(direction_string):]
     if direction_string in ['NULL', 'N']:
         direction_hexes = []
     else:
