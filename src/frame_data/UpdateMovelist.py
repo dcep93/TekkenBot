5255507eb25daa3889cabf75dd1a5c47a70bb977
Dan Bootcamp <dcep93@mail.com>
2020-09-12 18:55:50 -0700

    updates

--- src/frame_data/UpdateMovelist.py
+++ src/frame_data/UpdateMovelist.py
@@ -1,5 +1,7 @@
 from . import Database
 
+from misc import Path
+
 def update(char_name, move_nodes):
     try:
         update_helper(char_name, move_nodes)
@@ -8,22 +10,27 @@ def update(char_name, move_nodes):
 
 def update_helper(char_name, move_nodes):
     print('updating moves for', char_name)
-    start = Database.raw_moves[Database.Characters(char_name)]
+    char = Database.Characters(char_name)
+    start = Database.raw_moves[char]
     names_to_row = {i[-1]:i for i in start if i}
-    updated = False
+    count = 0
     for node in move_nodes:
         row = names_to_row.get(node.name)
         if row is None:
             continue
-        move_id = row[0]
+        move_ids = row[0].split(',')
         node_move_id = str(node.move_id)
-        if node_move_id != move_id:
+        if node_move_id not in move_ids:
             print('mismatch', char_name, node_move_id, node.name)
             print(' '.join(row))
             row[0] = node_move_id
-            updated = True
-    if updated:
-        csv_content = '\n'.join(['\t'.join(i) for i in start])
+            count += 1
+    print('diff', count)
+    if count > 0:
+        csv_content = '\n'.join(['\t'.join(i) for i in start])+'\n'
+        path = Path.path('./database/%s.csv' % char.name)
+        with open(path, 'w', encoding='UTF-8') as fh:
+            fh.write(csv_content)
     print('done')
 
 def update_move(move_id, move_name):
