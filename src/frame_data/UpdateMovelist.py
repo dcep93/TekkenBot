231aab37aad2223ac8ce385a6f1cac9f877ec018
Dan Bootcamp <dcep93@mail.com>
2020-12-05 18:15:51 -0800

    update

--- src/frame_data/UpdateMovelist.py
+++ src/frame_data/UpdateMovelist.py
@@ -12,19 +12,20 @@ def update_helper(char_name, move_nodes):
     print('updating moves for', char_name)
     char = Database.Characters(char_name)
     start = Database.raw_moves[char]
-    names_to_row = {i[-1]:i for i in start if i}
     count = 0
-    for node in move_nodes:
-        row = names_to_row.get(node.name)
-        if row is None:
-            continue
-        move_ids = row[0].split(',')
-        node_move_id = str(node.move_id)
-        if node_move_id not in move_ids:
-            print('mismatch', char_name, node_move_id, node.name)
-            print(' '.join(row))
-            row[0] = node_move_id
+    if True:
+        (key_in, key_out) = 0, -1
+        key_to_val = {str(i.move_id): i.name for i in move_nodes}
+    else:
+        (key_in, key_out) = -1, 0
+        key_to_val = {i.name: str(i.move_id) for i in move_nodes}
+    for row in start:
+        val_in = row[key_in].split(',')
+        val_out = ','.join([j for j in [key_to_val.get(i) for i in val_in] if j])
+        check_out = row[key_out]
+        if check_out != val_out:
             count += 1
+            row[key_out] = val_out
     print('diff', count)
     if count > 0:
         csv_content = '\n'.join(['\t'.join(i) for i in start])+'\n'
