fdfd5fa5d23a15b7c59567f84115a8894e28c47d
dcep93 <dcep93@gmail.com>
2024-03-25 18:30:25 -0400

    x

--- .github/workflows/build_zip.sh
+++ .github/workflows/build_zip.sh
@@ -1,7 +1,7 @@
 set -euo pipefail
 
 pip install pyinstaller
-python -m PyInstaller --noconfirm --onefile --windowed --clean --icon=assets/img/tekken_bot_close.ico --name TekkenBotPrime main.py
+python -m PyInstaller --debug --noconfirm --onefile --windowed --clean --icon=assets/img/tekken_bot_close.ico --name TekkenBotPrime main.py
 
 mv dist/TekkenBotPrime ./TekkenBotPrime.exe
 
