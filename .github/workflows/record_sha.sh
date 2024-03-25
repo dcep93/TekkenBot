8d024789f632a1e5a52cbbebe98bceb8b0204e64
dcep93 <dcep93@gmail.com>
2024-03-25 18:14:27 -0400

    x

--- .github/workflows/record_sha.sh
+++ .github/workflows/record_sha.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-printf "sha = '%s %s'\n" "$(git log -1)" "$(TZ='America/New_York' date)" >.github/workflows/recorded_sha.py
+printf "sha = '%s %s'\n" "$(git log -1 --format=format:%H)" "$(TZ='America/New_York' date)" >.github/workflows/recorded_sha.py
 cat .github/workflows/recorded_sha.py
